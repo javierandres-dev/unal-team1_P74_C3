61c07af44bdbaa644d111cf8399182d9786b9a3d
Edward Guevara <edarague@gmail.com>
2021-10-07 21:44:51 -0500

    .

--- src/main/java/com/miempresa/aplicacion/controladores/ControladorFactura.java
+++ src/main/java/com/miempresa/aplicacion/controladores/ControladorFactura.java
@@ -32,21 +32,6 @@ public class ControladorFactura {
         return "vistaFactura";
     }
     
-    @GetMapping("/crear/facturas") //path del controlador
-    public String crearVendedor(Model model){
-        model.addAttribute("vendedor",new Vendedor());
-        return "vistaCrearVendedor";
-    }        
-    @PostMapping("/crear/facturas")
-    public RedirectView procesarVendedor(@ModelAttribute Vendedor vendedor) {
-        Vendedor vendedorGuardado;
-        vendedorGuardado = repositorioVendedor.save(vendedor);
-        if (vendedorGuardado == null){
-            return new RedirectView("/crear/vendedor", true);
-        }
-        return new RedirectView("/vendedores/"+vendedorGuardado.getCodVendedor(),true);
-    }
-    
     @GetMapping("/crear/factura") //path del controlador
     public String crearFactura(Model model){
         model.addAttribute("factura",new Factura());
